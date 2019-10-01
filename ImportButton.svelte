8da8027193c2a539204ba4f1d867c75335abc476
Artur Mustafin <me@experimentalcommunity.org>
2019-10-01 10:24:48 +0300

    fix

--- ImportButton.svelte
+++ ImportButton.svelte
@@ -1,22 +1,22 @@
 <script>
-		import { createEventDispatcher } from "svelte";
-		import { click, saveAs } from "./js/io";
+	import { createEventDispatcher } from "svelte";
+	import { click, saveAs } from "./js/io";
 
-		export let id = "";
-		export let value = "";
+	export let id = "";
+	export let value = "";
 
-		let upload_json;
-		function loadContent() {
-		  click(upload_json);
-		}
+	let upload_json;
+	function loadContent() {
+	  click(upload_json);
+	}
 
-		const dispatch = createEventDispatcher();
-		function handleChange() {
-		  if (upload_json.files.length === 1) {
-		    dispatch("file", { file: upload_json.files[0] });
-		    upload_json.value = "";
-		  }
-		}
+	const dispatch = createEventDispatcher();
+	function handleChange() {
+	  if (upload_json.files.length === 1) {
+	    dispatch("file", { file: upload_json.files[0] });
+	    upload_json.value = "";
+	  }
+	}
 </script>
 
 <div hidden>
